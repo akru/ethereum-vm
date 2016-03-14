c5fcf838c5995a0aacf18fd325c177ebdc60bd3f
kejace <kejace@gmail.com>
2016-03-14 18:05:52 -0400

    Added SHA verification as option

--- src/Blockchain/VMOptions.hs
+++ src/Blockchain/VMOptions.hs
@@ -2,6 +2,8 @@
 
 module Blockchain.VMOptions where
 
+import Blockchain.Mining
+
 import HFlags
 
 defineFlag "debug" False "turn debug info on or off"
@@ -11,3 +13,5 @@ defineFlag "createTransactionResults" False "stores transaction results in the S
 defineFlag "sqlDiff" True "runs sqlDiff and updates account state and storage in SQL DB"
 defineFlag "queryBlocks" (10000::Int) "Number of blocks to query from SQL to process in one batch"
 defineFlag "miningVerification" True "Flag to turn mining verification or/off"
+
+defineEQFlag "miner" [| Instant :: MinerType |] "MINER" "What mining algorithm"
