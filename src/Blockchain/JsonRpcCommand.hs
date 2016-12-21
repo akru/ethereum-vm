8468574fad084bae9c92b674b78a26e492487047
Jamshid <qq@qq.com>
2016-12-21 01:07:56 -0500

    adding more json rpc calls

--- src/Blockchain/JsonRpcCommand.hs
+++ src/Blockchain/JsonRpcCommand.hs
@@ -20,6 +20,7 @@ import Blockchain.Data.Address
 import Blockchain.Data.AddressStateDB
 import Blockchain.Data.DataDefs
 import Blockchain.DB.AddressStateDB
+import Blockchain.DB.CodeDB
 import Blockchain.DB.DetailsDB
 import Blockchain.DB.HashDB
 import Blockchain.DB.SQLDB
@@ -39,7 +40,7 @@ produceResponse id theData = do
         Right resps -> return ()
 
 
-runJsonRpcCommand::(MonadLogger m, HasStateDB m, HasHashDB m, HasSQLDB m)=>
+runJsonRpcCommand::(MonadLogger m, HasStateDB m, HasHashDB m, HasSQLDB m, HasCodeDB m)=>
                    JsonRpcCommand->m ()
 runJsonRpcCommand c@JRCGetBalance{jrcAddress=address, jrcBlockString=blockString, jrcId=id} = do
   liftIO $ putStrLn $ "running command: " ++ show c
@@ -49,3 +50,33 @@ runJsonRpcCommand c@JRCGetBalance{jrcAddress=address, jrcBlockString=blockString
   let response = show $ addressStateBalance addressState
   liftIO $ produceResponse id $ BC.pack response
   liftIO $ putStrLn response
+
+runJsonRpcCommand c@JRCGetCode{jrcAddress=address, jrcBlockString=blockString, jrcId=id} = do
+  liftIO $ putStrLn $ "running command: " ++ show c
+  bestBlock <- getBestBlock
+  setStateDBStateRoot $ blockDataStateRoot $ blockBlockData bestBlock
+  addressState <- getAddressState address
+  maybeCode <- getCode $ addressStateCodeHash addressState
+  case maybeCode of
+   Just code -> liftIO $ produceResponse id code
+   Nothing -> liftIO $ produceResponse id ""
+
+runJsonRpcCommand c@JRCGetTransactionCount{jrcAddress=address, jrcBlockString=blockString, jrcId=id} = do
+  liftIO $ putStrLn $ "running command: " ++ show c
+  bestBlock <- getBestBlock
+  setStateDBStateRoot $ blockDataStateRoot $ blockBlockData bestBlock
+  addressState <- getAddressState address
+  let response = show $ addressStateNonce addressState
+  liftIO $ produceResponse id $ BC.pack response
+  liftIO $ putStrLn response
+
+{-
+runJsonRpcCommand c@JRCGetStorageAt{jrcAddress=address, jrcBlockString=blockString, jrcId=id} = do
+  liftIO $ putStrLn $ "running command: " ++ show c
+  bestBlock <- getBestBlock
+  setStateDBStateRoot $ blockDataStateRoot $ blockBlockData bestBlock
+  addressState <- getAddressState address
+  let response = show $ addressStateNonce addressState
+  liftIO $ produceResponse id $ BC.pack response
+  liftIO $ putStrLn response
+-}
